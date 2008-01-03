f64804d97098ce86c755762d09988d4938274542
s.clover <s.clover@gmail.com>
2008-01-03 10:02:50 +0100

    And untweaks
    darcs-hash:ce0389ce24533a04df01cbcba4749193ae17fcaf

--- Text/StringTemplate/Base.hs
+++ Text/StringTemplate/Base.hs
@@ -127,9 +127,10 @@ cacheSTGroup m g = unsafePerformIO $ go <$> newIORef M.empty
                          else return $ First . Just $ st)
                       (M.lookup s mp)
               where udReturn mp now = maybe (return $ First Nothing)
-                                      (atomicModifyIORef r .
-                                       ap ((>> return) . (flip (,) () .) .
-                                           M.insert s . (,) now) (First . Just))
+                                      (\st -> do
+                                         atomicModifyIORef r $
+                                          flip (,) () . M.insert s (now, st)
+                                         return $ First . Just $ st)
                                       (getFirst $ g s)
 
 --TODO: Lift out casing on time into the atomicModify call? 
