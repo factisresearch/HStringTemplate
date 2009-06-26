73c9e1649c3703cb83d1b107afaa7d3575afe018
so man3/gluBeginSurface.3 <none>
2009-06-27 01:26:04 +0200

    resolution, re-export SimpleGenerics
    darcs-hash:f174d0824a217505f73adb751cc1bf9502b71a1c

--- Text/StringTemplate/QQ.hs
+++ Text/StringTemplate/QQ.hs
@@ -16,17 +16,15 @@
 
 module Text.StringTemplate.QQ (stmp) where
 
-import Data.Generics
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
 import Text.StringTemplate.Base
-import Text.ParserCombinators.Parsec
 import Control.Monad.Writer
-import Control.Applicative
 
 quoteTmplExp :: String -> TH.ExpQ
 quoteTmplPat :: String -> TH.PatQ
 
+stmp :: QuasiQuoter
 stmp = QuasiQuoter quoteTmplExp quoteTmplPat
 
 quoteTmplPat = error "Cannot apply stmp quasiquoter in patterns"
@@ -34,10 +32,11 @@ quoteTmplExp s = return tmpl
   where
     vars = case parseSTMPNames s of
              Right xs -> xs
-             Left  err -> error "err"
+             Left  err -> error $ show err
     base  = TH.AppE (TH.VarE (TH.mkName "newSTMP")) (TH.LitE (TH.StringL s))
     tmpl  = foldr addAttrib base vars
     addAttrib var = TH.AppE
         (TH.AppE (TH.AppE (TH.VarE (TH.mkName "setAttribute"))
                           (TH.LitE (TH.StringL ('`' : var ++ "`"))))
                  (TH.VarE (TH.mkName  var)))
+
