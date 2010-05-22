c14bed6a2f3a259fbf773ff548fd6dc5d3ee79bd
s.clover <s.clover@gmail.com>
2010-05-22 02:08:15 +0200

    upgrade tests to qc2
    Ignore-this: 5edd4100a7be2332c740df5919bf8e8c
    
    darcs-hash:c92b536b3ecfc365aa736bf0f4c64befa97a57b1

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -19,17 +19,17 @@ import Text.StringTemplate.Group
 import Test.QuickCheck
 import System.Environment
 
---import Debug.Trace
---mytrace h x = trace (h ++ ": " ++ x) $ x
 
 main :: IO ()
 main = do
     args <- getArgs
     let n = if null args then 100 else read (head args)
-    (results, passed) <- mapAndUnzipM (\ (s, a) -> printf "%-25s: " s >> a n) tests
-    printf "Passed %d tests!\n" (sum passed)
-    when (not . and $ results) $ fail "Not all tests passed!"
+    results <- mapM (\ (s, a) -> printf "%-25s: " s >> fmap (\x->(s,x)) a) tests
+    mapM print results
+    when (not $ all (isSuccess  . snd) results) $ fail "Not all tests passed!"
  where
+    isSuccess (Success _) = True
+    isSuccess _ = False
     tests =
         [("prop_paddedTrans" , mytest prop_paddedTrans),
          ("prop_constStr" , mytest prop_constStr),
@@ -42,7 +42,7 @@ main = do
          ("prop_ifelse" , mytest prop_ifelse),
          ("prop_simpleGroup" , mytest prop_simpleGroup)
         ]
-
+mytest x = quickCheckResult x
 {-----------------------------------------------------------------------
   Limited tests for now: just for list juggling and some basic parsing.
 -----------------------------------------------------------------------}
@@ -61,36 +61,42 @@ prop_paddedTrans (x::[Int]) (y::[Int]) (z::[Int]) n =
 prop_constStr (LitString x) = x == (toString . newSTMP $ x)
 
 prop_emptyNulls (LitString x) (LitString y) i =
-    (concat . replicate (abs i) $ x) ==
-      (toString . newSTMP . concat . replicate (abs i) $ tmpl)
+    (concat . replicate i' $ x) ==
+      (toString . newSTMP . concat . replicate i' $ tmpl)
     where tmpl = x++"$"++y++"$"
+          i' = min (abs i) 10
 
 prop_fullNulls (LitString x) (LitString y) i =
     length y > 0 ==>
-               (concat . replicate (abs i) $ x++y) ==
-               (toString . newSTMP . concat . replicate (abs i) $ tmpl)
+               (concat . replicate i' $ x++y) ==
+               (toString . newSTMP . concat . replicate i' $ tmpl)
     where tmpl = x++"$"++y++";null='"++y++"'$"
+          i' = min (abs i) 10
 
 prop_substitution (LitString x) (LitString y) (LitString z) i =
     length y > 0 ==>
-               (concat . replicate (abs i) $ x++z) ==
+               (concat . replicate i' $ x++z) ==
                (toString . setAttribute y z .
-                newSTMP . concat . replicate (abs i) $ tmpl)
+                newSTMP . concat . replicate i' $ tmpl)
     where tmpl = x++"$"++y++"$"
+          i' = min (abs i) 10
 
 prop_separator (LitString x) (LitString y) (LitString z) i =
     length x > 0 ==>
-               (concat . intersperse z . replicate (abs i) $ y) ==
-               (toString . setAttribute x (replicate (abs i) y)
+               (concat . intersperse z . replicate i' $ y) ==
+               (toString . setAttribute x (replicate i' y)
                 . newSTMP $ tmpl)
     where tmpl = "$"++x++";separator='"++z++"'$"
+          i' = min (abs i) 10
 
 prop_comment (LitString x) (LitString y) (LitString z) =
     toString (newSTMP (x ++ "$!" ++ y ++ "!$" ++ z)) == x ++ z
 
 prop_attribs (LitString x) i =
-    toString (setManyAttrib (replicate (abs i) ("f",x)) $ newSTMP "$f$")
-                 == (concat . replicate (abs i) $ x)
+    toString (setManyAttrib (replicate i' ("f",x)) $ newSTMP "$f$")
+                 == (concat . replicate i' $ x)
+        where
+          i' = min (abs i) 10
 
 prop_ifelse a b c d =
     toString (setManyAttrib alist . newSTMP $ "$if(a)$a$elseif(b)$b$elseif(c)$c$else$$if(d)$d$else$e$endif$$endif$") == (fst . head . filter snd) alist
@@ -109,56 +115,7 @@ prop_simpleGroup (LitString x) (LitString y) (LitString z) (LitString t) =
 newtype LitChar = LitChar {unLitChar :: Char} deriving Show
 instance Arbitrary LitChar where
   arbitrary = LitChar <$> choose ('a','z')
-  coarbitrary = undefined
 
 newtype LitString = LitString String deriving Show
 instance Arbitrary LitString where
   arbitrary = LitString . map unLitChar <$> sized (\n -> choose (0,n) >>= vector)
-  coarbitrary = undefined
-{--------------------------------------------------------------------
-  QuickCheck Driver: This lovely code borrowed wholesale from XMonad.
---------------------------------------------------------------------}
-
-mytest :: Testable a => a -> Int -> IO (Bool, Int)
-mytest a n = mycheck defaultConfig
-    { configMaxTest=n
-    , configEvery   = \n args -> let s = show n in s ++ [ '\b' | _ <- s ] } a
-
-mycheck :: Testable a => Config -> a -> IO (Bool, Int)
-mycheck config a = do
-    rnd <- newStdGen
-    mytests config (evaluate a) rnd 0 0 []
-
-mytests :: Config -> Gen Result -> StdGen -> Int -> Int -> [[String]] -> IO (Bool, Int)
-mytests config gen rnd0 ntest nfail stamps
-    | ntest == configMaxTest config = done "OK," ntest stamps >> return (True, ntest)
-    | nfail == configMaxFail config = done "Arguments exhausted after" ntest stamps >> return (True, ntest)
-    | otherwise               =
-      do putStr (configEvery config ntest (arguments result)) >> hFlush stdout
-         case ok result of
-           Nothing    ->
-             mytests config gen rnd1 ntest (nfail+1) stamps
-           Just True  ->
-             mytests config gen rnd1 (ntest+1) nfail (stamp result:stamps)
-           Just False ->
-             putStr ( "Falsifiable after "
-                   ++ show ntest
-                   ++ " tests:\n"
-                   ++ unlines (arguments result)
-                    ) >> hFlush stdout >> return (False, ntest)
-     where
-      result      = generate (configSize config ntest) rnd2 gen
-      (rnd1,rnd2) = split rnd0
-
-done :: String -> Int -> [[String]] -> IO ()
-done mesg ntest stamps = putStr ( mesg ++ " " ++ show ntest ++ " tests" ++ table )
-  where
-    table = display . map entry . reverse . sort . map pairLength . group
-            . sort . filter (not . null) $ stamps
-    display []  = ".\n"
-    display [x] = " (" ++ x ++ ").\n"
-    display xs  = ".\n" ++ unlines (map (++ ".") xs)
-    pairLength xss@(xs:_) = (length xss, xs)
-    entry (n, xs)         = percentage n ntest ++ " "
-                            ++ concat (intersperse ", " xs)
-    percentage n m        = show ((100 * n) `div` m) ++ "%"
